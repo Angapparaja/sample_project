282ed76b48e5a47ad87398531ce7e4182f9720fd
Angapparaja <angapparaja494@gmail.com>
2021-09-06 10:21:39 +0530

    update react signin page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component,Fragment } from 'react'
+import {Container,Row,Col,Form,Button}from 'react-bootstrap';
+import { ArrowRight } from 'react-bootstrap-icons';
+import { FaUserAlt } from "react-icons/fa";
+import { BsLockFill } from "react-icons/bs";
+import './Style.css';
+import iconImg from './logo.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        
+        <Container>
+      
+        <img className="icon-img" src={iconImg} alt="icon"/>
+          <Row className="mt-5" >
+
+            <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-lg roundrd-lg ">
+           
+
+            <h3> welcome back </h3> 
+            <h5> Enter your credentials to access your account </h5> 
+           
+            <div className="lobox p-2">
+       <Form>
+      
+           <Form.Group >
+           
+             <Form.Control  type="email"  placeholder="Enter email" />
+             <FaUserAlt aria-hidden="true"/>
+              </Form.Group>
+
+                <Form.Group >
+              
+                <Form.Control type="password" placeholder="Password" />
+                <BsLockFill />
+                </Form.Group>
+                
+           
+             
+  <Button variant="primary btn-block" type="submit">
+        Signin
+  </Button>
+ <div className="text-left mt-3">
+   <a href="#"><small className="reset ">forget your password?</small></a>
+   <a href="#"><small className="resset ">reset password</small></a>
+ </div>
+</Form>
+</div>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+     
+    )
+  }
 }
 
-export default App;
+export default App
